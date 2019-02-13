08ea4558c9044fe1ddab48c579d90d2286d11fd2
Ryan Foster <phasecorex@gmail.com>
2019-02-13 18:38:33 -0500

    Turns out we need --upgrade so that arm32 works

--- root/start-redbot.sh
+++ root/start-redbot.sh
@@ -2,13 +2,11 @@
 set -e
 
 mkdir -p /data/venv
-
+python -m venv --upgrade /data/venv
 python -m venv /data/venv
-
 source /data/venv/bin/activate
 
 pip install -U pip
-
 pip install -U Red-DiscordBot
 
 exec redbot docker
