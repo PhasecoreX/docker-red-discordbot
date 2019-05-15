cd0ad2ec8d890fa2b67e00df2c10f52bc70494c4
Ryan Foster <phasecorex@gmail.com>
2019-05-15 18:01:50 -0400

    Fix #3
    I don't love it, but it is the only solution I can think of that fixes updating from a clean 3.0.2 as well as a botched 3.1.1, without doing a --force-reinstall every time. But I suppose in the far future I could remove this.

--- root/start-redbot.sh
+++ root/start-redbot.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env sh
 set -e
 
+# discord.py doesn't update properly to 1.0.1, so we are forced to delete the venv
+if [ ! -f "/data/venv/pcxupdates.txt" ]; then
+    rm -rf /data/venv
+    mkdir -p /data/venv
+    echo "1" > "/data/venv/pcxupdates.txt"
+fi
+
 mkdir -p /data/venv
 python -m venv --upgrade /data/venv
 python -m venv /data/venv
 source /data/venv/bin/activate
 
-pip install -U pip
-pip install -U Red-DiscordBot
+python -m pip install --upgrade --no-cache-dir pip
+python -m pip install --upgrade --no-cache-dir Red-DiscordBot
 
 exec redbot docker
