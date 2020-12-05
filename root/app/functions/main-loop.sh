0716f2d9032357b4b4d42bdee508a4983a6cc943
Ryan Foster <phasecorex@gmail.com>
2020-12-05 10:23:03 -0500

    Add more UpdateNotify cog support, potentially fix redbot permission denied errors on startup

--- root/app/functions/main-loop.sh
+++ root/app/functions/main-loop.sh
@@ -54,23 +54,23 @@ while [ "${RETURN_CODE}" -eq 26 ]; do
 
     if [ -n "${TOKEN:-}" ]; then
         echo "Setting bot token..."
-        redbot docker --edit --no-prompt --token "${TOKEN}"
+        python -m redbot docker --edit --no-prompt --token "${TOKEN}"
     fi
 
     if [ -n "${PREFIXES}" ]; then
         echo "Setting bot prefix(es)..."
-        redbot docker --edit --no-prompt ${PREFIXES}
+        python -m redbot docker --edit --no-prompt ${PREFIXES}
     fi
 
     echo "Starting Red-DiscordBot!"
     set +e
     # If we are running in an interactive shell, we can't (and don't need to) do any of the fancy interrupt catching
     if [ -t 0 ]; then
-        redbot docker ${EXTRA_ARGS:-}
+        python -m redbot docker ${EXTRA_ARGS:-}
         RETURN_CODE=$?
     else
         prep_term
-        redbot docker ${EXTRA_ARGS:-} &
+        python -m redbot docker ${EXTRA_ARGS:-} &
         wait_term
         RETURN_CODE=$?
     fi
