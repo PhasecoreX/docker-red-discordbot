32655c70b5b6252c4a3acd808a0b343e0f791310
Draper <27962761+Drapersniper@users.noreply.github.com>
2023-04-25 12:25:39 +0100

    Clone PyLav on Postgres config drivers (Requires a one-off usage of `[p]addpath /data/pylav/cogs`

--- root/app/functions/update-redbot.sh
+++ root/app/functions/update-redbot.sh
@@ -9,12 +9,9 @@ if [ -z "${STORAGE_TYPE:-}" ]; then
     STORAGE_TYPE=$(jq -r .docker.STORAGE_TYPE /data/config.json | tr '[:upper:]' '[:lower:]')
 fi
 SETUPTOOLS_EXTRAS=""
-if [ "${STORAGE_TYPE}" != "json" ]; then
-    SETUPTOOLS_EXTRAS="[${STORAGE_TYPE}]"
-else
-  # Install/Update PyLav for JSON configs
-  python /app/functions/pylav_setup.py
-fi
+
+# Clone/Pull/Install/Update PyLav Cogs
+python /app/functions/pylav_setup.py
 
 if [ -z "${PYLAV__DOCKER_DEV_SKIP_INSTALL:-}" ]; then
   # Clone/Pull/Install/Update PyLav Cogs
