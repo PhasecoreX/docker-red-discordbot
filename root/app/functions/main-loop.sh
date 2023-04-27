3b9043d72727f7661cc4997f2ccf1ef6666852de
Ryan Foster <phasecorex@gmail.com>
2023-04-27 17:47:10 -0400

    Last commit to support Python 3.9
    Only use `--upgrade-strategy eager` if needed

--- root/app/functions/main-loop.sh
+++ root/app/functions/main-loop.sh
@@ -44,10 +44,12 @@ while [ "${RETURN_CODE}" -eq 26 ]; do
     set +e
     # If we are running in an interactive shell, we can't (and don't need to) do any of the fancy interrupt catching
     if [ -t 0 ]; then
+        # shellcheck disable=SC2086
         python -O -m redbot docker ${EXTRA_ARGS:-}
         RETURN_CODE=$?
     else
         prep_term
+        # shellcheck disable=SC2086
         python -O -m redbot docker ${EXTRA_ARGS:-} &
         wait_term
         RETURN_CODE=$?
--- root/app/functions/update-redbot.sh
+++ root/app/functions/update-redbot.sh
@@ -16,12 +16,17 @@ fi
 if [ -n "${CUSTOM_REDBOT_PACKAGE:-}" ]; then
     echo "WARNING: You have specified a custom Red-DiscordBot Pip install. Little to no support will be given for this setup."
     echo "Updating Red-DiscordBot with \"${CUSTOM_REDBOT_PACKAGE}\"..."
-    python -m pip install --upgrade --no-cache-dir pip setuptools wheel
-    python -m pip install --upgrade --upgrade-strategy eager --no-cache-dir "${CUSTOM_REDBOT_PACKAGE}"
+    python -m pip install --upgrade --upgrade-strategy eager --no-cache-dir wheel "${CUSTOM_REDBOT_PACKAGE}"
+    echo "${CUSTOM_REDBOT_PACKAGE}" >"/data/venv/.redbotversion"
 else
     # Update redbot
     REDBOT_PACKAGE_NAME="Red-DiscordBot${SETUPTOOLS_EXTRAS}${REDBOT_VERSION:-}"
+    UPGRADE_STRATEGY=""
+    if [ ! -f "/data/venv/.redbotversion" ] || [ "$(cat "/data/venv/.redbotversion")" != "${REDBOT_PACKAGE_NAME}" ]; then
+        UPGRADE_STRATEGY="--upgrade-strategy eager"
+    fi
     echo "Updating ${REDBOT_PACKAGE_NAME}..."
-    python -m pip install --upgrade --no-cache-dir pip setuptools wheel
-    python -m pip install --upgrade --upgrade-strategy eager --no-cache-dir "${REDBOT_PACKAGE_NAME}"
+    # shellcheck disable=SC2086
+    python -m pip install --upgrade ${UPGRADE_STRATEGY} --no-cache-dir wheel "${REDBOT_PACKAGE_NAME}"
+    echo "${REDBOT_PACKAGE_NAME}" >"/data/venv/.redbotversion"
 fi
